d32c88dd3c2690d5de099291ed82956af673d2fb
yhwach <2danilaproduction@gmail.com>
2025-09-20 18:35:18 +0700

    Reapply "Создание класса Entity"
    This reverts commit b17296f5339d693cb395910bb726114421d76669.

--- aqs/aqs.cpp
+++ aqs/aqs.cpp
@@ -1,4 +1,32 @@
 ﻿#include <iostream>
+#include <string>
+
+class Entity {
+protected:
+	float x, y; // Координаты объекта
+	std::string name; // Имя объекта
+public:
+	Entity(float x_pos, float y_pos, std::string obj_name)
+		: x(x_pos), y(y_pos), name(obj_name) {}
+	void Move(float dx, float dy) {
+		x += dx;
+		y += dy;
+	}
+
+	void Print() const {  // Вывод информации
+		std::cout << name << " at (" << x << ", " << y << ")" << std::endl;
+	}
+
+	float getX() const {
+		return x;
+	}
+	float getY() const {
+		return y;
+	}
+	std::string getName() const {
+		return name;
+	}
+};
 
 int main()
 {
