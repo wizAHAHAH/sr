609185686d70c9f9048bd93b9350612ed9f78e8b
yhwach <2danilaproduction@gmail.com>
2025-09-20 18:43:40 +0700

    Создание класса Wizard

--- aqs/aqs.cpp
+++ aqs/aqs.cpp
@@ -28,6 +28,31 @@ public:
 	}
 };
 
+class Wizard : public Entity {
+private:
+	int mana;
+	int health;
+public:
+	Wizard(float x_pos, float y_pos, std::string w_name, int w_mana, int w_health)
+		: Entity(x_pos, y_pos, w_name), mana(w_mana), health(w_health) {}
+	void castSpell() {
+		if (mana >= 10) {
+			mana -= 10;
+			std::cout << name << " использование заклинания!" << std::endl;
+		}
+		else {
+			std::cout << name << " недостаточно маны!" << std::endl;
+		}
+	}
+
+	int getMana() const {
+		return mana;
+	}
+	int getHealth() const {
+		return health;
+	}
+};
+
 int main()
 {
  
